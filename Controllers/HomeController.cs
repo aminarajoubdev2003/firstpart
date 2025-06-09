78b59cc26ca66b048da9d27daf24b8120ab11a1a
aminarajoubdev2003 <amina.rajoub03@gmail.com>
2025-06-09 12:33:18 +0300

    Added (Edit Task) feature

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -49,5 +49,20 @@ namespace MvcTodoApp.Controllers
                 task.IsComplete = true;
             return RedirectToAction("Index");
         }
+
+        /// <summary>
+        /// تعديل عنوان مهمة.
+        /// </summary>
+        [HttpPost]
+        public IActionResult EditTask(int id, string newTitle)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null && !string.IsNullOrEmpty(newTitle))
+            {
+                task.Title = newTitle;
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
